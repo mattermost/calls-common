0d29d55b794cdbedfb704307deff272b17dd2012
streamer45 <cstcld91@gmail.com>
2023-03-24 12:51:31 -0600

    Commit types

--- lib/types/types.d.ts
+++ lib/types/types.d.ts
@@ -135,6 +135,7 @@ export type CallsConfig = {
     EnableRecordings: boolean;
     MaxRecordingDuration: number;
     sku_short_name: string;
+    EnableSimulcast: boolean;
 };
 export type Reaction = UserReactionData & {
     displayName: string;
