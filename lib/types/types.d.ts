3be771bd8a14eff439830571f80544513fa81c52
Christopher Poile <cpoile@gmail.com>
2023-05-10 17:23:10 -0400

    lowercase to indicate it's not a config setting

--- lib/types/types.d.ts
+++ lib/types/types.d.ts
@@ -83,7 +83,7 @@ export type CallsConfig = {
     MaxRecordingDuration: number;
     sku_short_name: string;
     EnableSimulcast: boolean;
-    EnableRTCD: boolean;
+    enableRTCD: boolean;
 };
 export type Reaction = UserReactionData & {
     displayName: string;
