7e8e818a7c1cd5e4b7a121170c12d208c834b8d9
Christopher Poile <cpoile@gmail.com>
2024-05-10 17:02:55 -0400

    [MM-57919] Add host control message types (#30)
    * add host control message types
    
    * send channel_id with removed msg

--- src/types/types.ts
+++ src/types/types.ts
@@ -81,6 +81,7 @@ export type UserReactionData = {
 
 export type CallHostChangedData = {
     hostID: string;
+    call_id: string;
 } & BaseData
 
 export type CallJobState = {
@@ -286,3 +287,19 @@ export type CallRecordingPostProps = {
     recording_id: string;
     captions: CallCaption[];
 };
+
+export type HostControlMsg = {
+    channel_id: string;
+    session_id: string;
+}
+
+export type HostControlLowerHand = HostControlMsg & {
+    call_id: string;
+    host_id: string;
+}
+
+export type HostControlRemoved = HostControlMsg & {
+    call_id: string;
+    user_id: string;
+}
+
